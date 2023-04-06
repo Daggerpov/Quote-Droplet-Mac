dcfd327ae46e7e22323d2df8715ccfa412eba854
Daggerpov <danielagapov1@gmail.com>
2023-04-05 19:32:41 -0700

    Initial Commit

---
+++ Quote-Droplet/Quote_DropletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_DropletApp.swift
+//  Quote-Droplet
+//
+//  Created by Daniel Agapov on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct Quote_DropletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
